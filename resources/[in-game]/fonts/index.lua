77880311c80fe177a906147e788a09c5915d491f
mahlukat5 <temmuz.sanaga@hotmail.com>
2023-02-23 00:02:04 +0300

    fontlar için cache oluşturuldu ve font listesi düzenlendi

--- resources/[in-game]/fonts/index.lua
+++ resources/[in-game]/fonts/index.lua
@@ -1,20 +1,24 @@
 fonts = {
     list = {
-        {'RobotoB', 'assets/RobotoB.ttf'},
-        {'Roboto', 'assets/Roboto.ttf'},
+        ['RobotoB']= 'assets/RobotoB.ttf',
+        ['Roboto']= 'assets/Roboto.ttf',
     },
+
+    -- oluşturulan fontlar
+    font_cache = {},
     
     get = function(self,name,size)
-        if not size then
-            size = 9
-        end
-        self.font = nil
-        for index, value in ipairs(self.list) do
-            if value[1] == name then
-                self.font = DxFont(value[2], size)
-            end
-        end
-        return self.font
+        local size = size or 9
+        local font_konum = self.list[name]
+
+        -- eğer cache'de belirtlilen isim  ve size'da font varsa, tekrar oluşturma.
+        if self.font_cache[name.." "..size] then return self.font_cache[name.." "..size] end
+
+        if font_konum then
+            self.font_cache[name.." "..size] = DxFont(font_konum, size)
+        end 
+
+        return self.font_cache[name.." "..size]
     end,
 
     index = function(self)
